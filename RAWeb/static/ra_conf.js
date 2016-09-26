1d630cc2bea1c614ffcb0f2a9dc438820d6792c5
jionghang.caijh <jionghang.caijh@alibaba-inc.com>
2016-09-26 18:58:27 +0800

    fix security issue

--- RAWeb/static/ra_conf.js
+++ RAWeb/static/ra_conf.js
@@ -1,9 +1,7 @@
 var ra_conf = {
-    //"api_url": "http://red_alert_web.proxy.taobao.org/",
-    //"api_url": "http://100.82.23.31:80/red_alert_web",
-    "api_url": "http://100.82.23.31:5011",
-    "current_url": "http://0.0.0.0:5011/index.html",
-    "buc_sso_url": "http://search-tools.yisou.com/buc_sso/index.php",
+    "api_url": "http://host:port",
+    "current_url": "http://host:port/index.html",
+    "buc_sso_url": "sso_url",
     "api_timeout": 3000,
     "admin_timeout": 10000,
     "disable_account": true
